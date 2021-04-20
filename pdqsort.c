293e57c6a1d1cf8b54297f9e97b215b6369fdbfa
hank <hankluo6@gmail.com>
2021-04-20 16:33:37 +0800

    Add branchless version

--- pdqsort.c
+++ pdqsort.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdalign.h>
 
 #define insertion_sort_threshold 24
 #define ninther_threshold 128
@@ -28,6 +29,11 @@ static int cmpint64(const void *a, const void *b)
     if (a_val == b_val)
         return 0;
     return -1;
+}
+
+static int cmpuint64(const void *a, const void *b)
+{
+    return *(uint64_t *)a < *(uint64_t *)b;
 }
 
 /**
@@ -117,6 +123,8 @@ static void swap_bytes(void *a, void *b, size_t n)
 #define SWAP_WORDS_32 (swap_func_t) 1
 #define SWAP_BYTES (swap_func_t) 2
 
+#define BLOCK_SIZE 64
+
 /*
  * The function pointer is last to make tail calls most efficient if the
  * compiler decides not to inline this function.
@@ -136,6 +144,7 @@ static void do_swap(void *a, void *b, size_t size, swap_func_t swap_func)
 #if DEBUG
 static void print(void *_begin, void *_end, size_t size)
 {
+    
     for (int i = 0, char *cur = (char *)_begin; cur != (char *)_end; cur += idx(1), i++) {
         printf("%d %lu\n", i, *(uint64_t *) cur);
     }
@@ -156,12 +165,12 @@ static void insertion_sort(void *_begin, void *_end, size_t size, cmp_func_t cmp
         char *sift = cur;
         char *sift_1 = cur - idx(1);
 
-        if (cmp_func(sift, sift_1) < 0) {
+        if (cmp_func(sift, sift_1)) {
             memcpy(tmp, sift, size);
             do {
                 memcpy(sift, sift_1, size);
                 sift -= idx(1);
-            } while (sift != begin && cmp_func(tmp, sift_1 -= idx(1)) < 0);
+            } while (sift != begin && cmp_func(tmp, sift_1 -= idx(1)));
             memcpy(sift, tmp, size);
         }
     }
@@ -182,12 +191,12 @@ static void unguarded_insertion_sort(void *_begin, void *_end, size_t size, cmp_
         char *sift = cur;
         char *sift_1 = cur - idx(1);
 
-        if (cmp_func(sift, sift_1) < 0) {
+        if (cmp_func(sift, sift_1)) {
             memcpy(tmp, sift, size);
             do {
                 memcpy(sift, sift_1, size);
                 sift -= idx(1);
-            } while (cmp_func(tmp, sift_1 -= idx(1)) < 0);
+            } while (cmp_func(tmp, sift_1 -= idx(1)));
             memcpy(sift, tmp, size);
         }
     }
@@ -209,12 +218,12 @@ bool partial_insertion_sort(void *_begin, void *_end, size_t size, cmp_func_t cm
         char *sift = cur;
         char *sift_1 = cur - idx(1);
 
-        if (cmp_func(sift, sift_1) < 0) {
+        if (cmp_func(sift, sift_1)) {
             memcpy(tmp, sift, size);
             do {
                 memcpy(sift, sift_1, size);
                 sift -= idx(1);
-            } while (sift != begin && cmp_func(tmp, sift_1 -= idx(1)) < 0);
+            } while (sift != begin && cmp_func(tmp, sift_1 -= idx(1)));
             memcpy(sift, tmp, size);
 
             limit += (cur - sift) / size;
@@ -228,7 +237,7 @@ bool partial_insertion_sort(void *_begin, void *_end, size_t size, cmp_func_t cm
 
 void sort2(void *a, void *b, size_t size, cmp_func_t cmp_func)
 {
-    if (cmp_func(b, a) < 0)
+    if (cmp_func(b, a))
         do_swap(a, b, size, 0);
 }
 
@@ -237,6 +246,149 @@ void sort3(void *a, void *b, void *c, size_t size, cmp_func_t cmp_func)
     sort2(a, b, size, cmp_func);
     sort2(b, c, size, cmp_func);
     sort2(a, b, size, cmp_func);
+}
+
+void swap_offsets(char *first, char *last, size_t size,
+                            char* offsets_l, char* offsets_r,
+                            size_t num, bool use_swaps) {
+    if (use_swaps) {
+        // This case is needed for the descending distribution, where we need
+        // to have proper swapping for pdqsort to remain O(n).
+        for (size_t i = 0; i < num; ++i) {
+            do_swap(first + idx(offsets_l[i]), last - idx(offsets_r[i]), size, 0);
+        }
+    } else if (num > 0) {
+        char *l = first + idx(offsets_l[0]);
+        char *r = last - idx(offsets_r[0]);
+        char *tmp = malloc(size);
+        memcpy(tmp, l, size);
+        memcpy(l, r, size);
+
+        for (size_t i = 1; i < num; ++i) {
+            l = first + idx(offsets_l[i]);
+            memcpy(r, l, size);
+            r = last - idx(offsets_r[i]); 
+            memcpy(l, r, size);
+        }
+        memcpy(r, tmp, size);
+        free(tmp);
+    }
+}
+
+bool partition_right_branchless(void *_begin, void *_end, size_t size, cmp_func_t cmp_func, char **ret_pivot)
+{
+    char *begin = (char *)_begin;
+    char *end = (char *)_end;
+    char *pivot = malloc(size);
+    memcpy(pivot, begin, size);
+
+    char *first = begin;
+    char *last = end;
+
+    while (cmp_func(first += idx(1), pivot))
+        ;
+
+    if (first - idx(1) == begin)
+        while (first < last && !cmp_func(last -= idx(1), pivot));
+    else
+        while (!cmp_func(last -= idx(1), pivot));
+
+    bool already_partitioned = first >= last;
+    if (!already_partitioned) {
+        do_swap(first, last, size, 0);
+        first += idx(1);
+
+        alignas(BLOCK_SIZE) char offsets_l_storage[BLOCK_SIZE];
+        alignas(BLOCK_SIZE) char offsets_r_storage[BLOCK_SIZE];
+
+        char *offsets_l = offsets_l_storage;
+        char *offsets_r = offsets_r_storage;
+
+        char *offsets_l_base = first;
+        char *offsets_r_base = last;
+        size_t num_l, num_r, start_l, start_r;
+        num_l = num_r = start_l = start_r = 0;
+        while (first < last) {
+            size_t num_unknown = (last - first) / size;
+            size_t left_split = num_l == 0 ? (num_r == 0 ? num_unknown / 2 : num_unknown) : 0;
+            size_t right_split = num_r == 0 ? (num_unknown - left_split) : 0;
+
+            if (left_split >= BLOCK_SIZE) {
+                for (size_t i = 0; i < BLOCK_SIZE; ) {
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                }
+            } else {
+                for (size_t i = 0; i < left_split; ) {
+                    offsets_l[num_l] = i++; num_l += !cmp_func(first, pivot); first += idx(1);
+                }
+            }
+
+            if (right_split >= BLOCK_SIZE) {
+                for (size_t i = 0; i < BLOCK_SIZE; ) {
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                }
+            } else {
+                for (size_t i = 0; i < right_split; ) {
+                    offsets_r[num_r] = ++i; last -= idx(1); num_r += cmp_func(last, pivot);
+                }
+            }
+
+            size_t num = num_l < num_r ? num_l : num_r;
+            swap_offsets(offsets_l_base, offsets_r_base, size,
+                            offsets_l + start_l, offsets_r + start_r,
+                            num, num_l == num_r);
+            num_l -= num;
+            num_r -= num;
+            start_l += num;
+            start_r += num;
+            if (!num_l) {
+                start_l = 0;
+                offsets_l_base = first;
+            }
+
+            if (!num_r) {
+                start_r = 0;
+                offsets_r_base = last;
+            }
+        }
+        // We have now fully identified [first, last)'s proper position. Swap the last elements.
+        if (num_l) {
+            offsets_l += start_l;
+            while (num_l--)
+                do_swap(offsets_l_base + idx(offsets_l[num_l]), (last -= idx(1)), size, 0);
+            first = last;
+        }
+        if (num_r) {
+            offsets_r += start_r;
+            while (num_r--) {
+                do_swap(offsets_r_base - idx(offsets_r[num_r]), first, size, 0);
+                first += idx(1);
+            }
+            last = first;
+        }
+    }
+    char *pivot_pos = first - idx(1);
+    memcpy(begin, pivot_pos, size);
+    memcpy(pivot_pos, pivot, size);
+    *ret_pivot = pivot_pos;
+    
+    free(pivot);
+
+    return pivot_pos;
 }
 
 bool partition_right(void *_begin, void *_end, size_t size, cmp_func_t cmp_func, char **ret_pivot)
@@ -249,21 +401,21 @@ bool partition_right(void *_begin, void *_end, size_t size, cmp_func_t cmp_func,
     char *first = begin;
     char *last = end;
 
-    while (cmp_func(first += idx(1), pivot) < 0)
+    while (cmp_func(first += idx(1), pivot))
         ;
 
     if (first - idx(1) == begin)
-        while (first < last && cmp_func(last -= idx(1), pivot) >= 0);
+        while (first < last && !cmp_func(last -= idx(1), pivot));
     else
-        while (cmp_func(last -= idx(1), pivot) >= 0);
+        while (!cmp_func(last -= idx(1), pivot));
 
     bool already_partitioned = first >= last;
 
     while (first < last) {
         do_swap(first, last, size, 0);
-        while (cmp_func(first += idx(1), pivot) < 0)
+        while (cmp_func(first += idx(1), pivot))
             ;
-        while (cmp_func(last -= idx(1), pivot) >= 0)
+        while (!cmp_func(last -= idx(1), pivot))
             ;
     }
     char *pivot_pos = first - idx(1);
@@ -287,19 +439,19 @@ char *partition_left(void *_begin, void *_end, size_t size, cmp_func_t cmp_func)
     char *first = begin;
     char *last = end;
 
-    while (cmp_func(pivot, (last -= idx(1))) < 0)
+    while (cmp_func(pivot, (last -= idx(1))))
         ;
 
     if (last + idx(1) == end)
-        while (first < last && cmp_func(pivot, (first += idx(1))) >= 0);
+        while (first < last && !cmp_func(pivot, (first += idx(1))));
     else
-        while (cmp_func(pivot, (first += idx(1))) >= 0);
+        while (!cmp_func(pivot, (first += idx(1))));
 
     while (first < last) {
         do_swap(first, last, size, 0);
-        while (cmp_func(pivot, (last -= idx(1))) < 0)
+        while (cmp_func(pivot, (last -= idx(1))))
             ;
-        while (cmp_func(pivot, (first += idx(1))) >= 0)
+        while (!cmp_func(pivot, (first += idx(1))))
             ;
     }
 
@@ -339,13 +491,12 @@ void pdqsort_loop(void *_begin,
         } else {
             sort3(begin, begin + idx(m), end - idx(1), size, cmp_func);
         }
-        if (!leftmost && cmp_func(begin - idx(1), begin) >= 0) {
+        if (!leftmost && !cmp_func(begin - idx(1), begin)) {
             begin = partition_left(begin, end, size, cmp_func) + idx(1);
             continue;
         }
-
         char *pivot;
-        bool already_partitioned = partition_right(begin, end, size, cmp_func, &pivot);
+        bool already_partitioned = partition_right_branchless(begin, end, size, cmp_func, &pivot);
     
         size_t l_size = (pivot - begin) / size;
         size_t r_size = (end - (pivot + idx(1))) / size;
@@ -465,7 +616,7 @@ int main()
     for (int i = 0; i < TEST_LEN; ++i) {
         arr[i] = rand();
     }
-    pdqsort(arr, TEST_LEN, sizeof(uint64_t), cmpint64, NULL);
+    pdqsort(arr, TEST_LEN, sizeof(uint64_t), cmpuint64, NULL);
     for (int i = 0; i < TEST_LEN - 1; ++i) {
         if (arr[i] > arr[i + 1]) {
             fprintf(stderr, "sort failed\n");
